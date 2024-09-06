e9a4d896c74fa2d3c77a04e81575d9cb84f2b781
Rainbow-Platypus <votre.email@exemple.com>
2024-09-06 16:41:18 +0200

    push de package.json

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,17 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['placeholder.com'],
+  },
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://backend:5000/api/:path*',
+        destination: 'http://backend:5000/api/:path*', // Proxy to Backend
       },
     ]
   },
 }
+
+module.exports = nextConfig
